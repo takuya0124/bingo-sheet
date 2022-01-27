e21649c82509cbd9971363410b5261c525b681d3
長谷川拓也 <takuya.hasegawa0124@gmail.com>
2022-01-27 09:15:23 +0900

    感想の追加

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello World!!',
     items: [
+      //list:ビンゴシートの数字の配列，isActiveList:有効なマスの判定
       {column:'B', list:[], isActiveList:[false,false,false,false,false]},
       {column:'I', list:[], isActiveList:[false,false,false,false,false]},
       {column:'N', list:[], isActiveList:[false,false,true,false,false]},
       {column:'G', list:[], isActiveList:[false,false,false,false,false]},
       {column:'O', list:[], isActiveList:[false,false,false,false,false]}
     ],
-    buttonState: false,
-    isReach : false,
-    isBingo : false,
-    CheckCount : [[0, 0, 1, 0, 0],[0, 0, 1, 0, 0],[1, 1, -1, -1, -1]]
+    isReach : false,//リーチの判定
+    isBingo : false,//ビンゴの判定
+    CheckCount : [[0, 0, 1, 0, 0],[0, 0, 1, 0, 0],[1, 1, -1, -1, -1]]//縦，横，斜めのどの列に何個有効なマスがあるかのカウント
   },
   methods:{
+    //マスが押されたときに実行される関数
     changeState: function(i,j){
       if(i!=j || i !=2){
         this.items[j].isActiveList.splice(i, 1, !this.items[j].isActiveList[i]);
@@ -42,17 +42,20 @@ var app = new Vue({
   }
 });
 
+//初めにビンゴシートの数字を生成する
 for (let i = 0; i < 5; i++){
   app.items[i].list = getRandomList(i*15+1,(i+1)*15,5);
   app.items[2].list[2] = 'free'
 }
 
+//min,maxの間の数字をランダムに生成
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+//min,maxの間の数字をindex個ランダムに配列を生成
 function getRandomList(min, max, index){
   randoms = [];
   for(i = 0; i < index; i++){
@@ -67,6 +70,7 @@ function getRandomList(min, max, index){
   return randoms;
 }
 
+//縦，横，斜めのマスの個数をカウントする
 function CheckReach(i,j,CheckCount,isActive){
   if(isActive){
     num = 1;
